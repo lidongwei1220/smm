ff4f01d8204afdfe2f049d80ba205f8ba462303f
unknown <1490619147@qq.com>
2019-11-08 16:58:04 +0800

    second

---
+++ src/main/java/com/example/smm/swagger/test/SwaggerWebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.smm.swagger.test;
+
+
+import com.example.smm.swagger.GlobalSwagger;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author lidongwei
+ * @date 2019-11-07
+ **/
+
+@EnableSwagger2
+@Configuration
+public class SwaggerWebMvcConfiguration extends GlobalSwagger {
+}
